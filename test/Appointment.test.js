ba03a81534a6a96874109a542114930debf1641f
Leon Linton <leon.linton@live.co.uk>
2019-09-16 14:44:31 +0100

    DRY's up Tests

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -3,21 +3,24 @@ import ReactDOM from "react-dom";
 import { Appointment } from "../src/Appointment";
 
 describe("Appointment", () => {
+  let container;
+  let client;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+  });
+
+  const render = component => ReactDOM.render(component, container);
+
   it("renders the client first name", () => {
-    const client = { firstName: "Ashley" };
-    const container = document.createElement("div");
-
-    ReactDOM.render(<Appointment client={client} />, container);
-
+    client = { firstName: "Ashley" };
+    render(<Appointment client={client} />);
     expect(container.textContent).toMatch("Ashley");
   });
 
   it("renders another client first name", () => {
-    const client = { firstName: "Jordan" };
-    const container = document.createElement("div");
-
-    ReactDOM.render(<Appointment client={client} />, container);
-
+    client = { firstName: "Jordan" };
+    render(<Appointment client={client} />);
     expect(container.textContent).toMatch("Jordan");
   });
 });
