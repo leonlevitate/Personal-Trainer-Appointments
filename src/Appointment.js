c8395a53881f02ec1d92d7de10ebee915a198e2f
Leon Linton <leon.linton@live.co.uk>
2019-09-17 15:05:00 +0100

    Renders multiple appointments in an li element

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+const appointmentTimeofDay = startsAt => {
+  const [h, m] = new Date(startsAt).toTimeString().split(":");
+  return `${h}:${m}`;
+};
+
 export const Appointment = ({ client: { firstName } }) => (
   <div>{firstName}</div>
 );
 
-export const AppointmentsDayView = () => <div id="appointmentsDayView"></div>;
+export const AppointmentsDayView = ({ appointments }) => (
+  <div id="appointmentsDayView">
+    <ol>
+      {appointments.map(appointment => (
+        <li key={appointment.startsAt}>
+          {appointmentTimeofDay(appointment.startsAt)}
+        </li>
+      ))}
+    </ol>
+  </div>
+);
