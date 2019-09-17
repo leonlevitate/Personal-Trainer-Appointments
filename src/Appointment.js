a1c2469662d9eb739aa40c89bac5f638c2fcb319
Leon Linton <leon.linton@live.co.uk>
2019-09-17 14:10:06 +0100

    Sets up AppointmentsDayView

--- src/Appointment.js
+++ src/Appointment.js
@@ -3,3 +3,5 @@ import React from "react";
 export const Appointment = ({ client: { firstName } }) => (
   <div>{firstName}</div>
 );
+
+export const AppointmentsDayView = () => <div id="appointmentsDayView"></div>;
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Appointment } from "../src/Appointment";
+import { Appointment, AppointmentsDayView } from "../src/Appointment";
 
 describe("Appointment", () => {
   let container;
@@ -22,5 +22,20 @@ describe("Appointment", () => {
     client = { firstName: "Jordan" };
     render(<Appointment client={client} />);
     expect(container.textContent).toMatch("Jordan");
+  });
+});
+
+describe("AppointmentsDayView", () => {
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+  });
+
+  const render = component => ReactDOM.render(component, container);
+
+  it("renders a div with the right id", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(container.querySelector("div#appointmentsDayView")).not.toBeNull();
   });
 });
