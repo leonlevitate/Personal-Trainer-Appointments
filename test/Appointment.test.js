748cc94ad46ded284c466d64c8ab41787282ee73
Leon Linton <leon.linton@live.co.uk>
2019-09-21 19:09:23 +0100

    Refactors the tests

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -27,6 +27,11 @@ describe("Appointment", () => {
 
 describe("AppointmentsDayView", () => {
   let container;
+  const today = new Date();
+  const appointments = [
+    { startsAt: today.setHours(12, 0) },
+    { startsAt: today.setHours(13, 0) }
+  ];
 
   beforeEach(() => {
     container = document.createElement("div");
@@ -40,22 +45,12 @@ describe("AppointmentsDayView", () => {
   });
 
   it("renders multiple appointments in an ol element", () => {
-    const today = new Date();
-    const appointments = [
-      { startsAt: today.setHours(12, 0) },
-      { startsAt: today.setHours(13, 0) }
-    ];
     render(<AppointmentsDayView appointments={appointments} />);
     expect(container.querySelector("ol")).not.toBeNull();
     expect(container.querySelector("ol").children).toHaveLength(2);
   });
 
   it("renders each appointment in an li", () => {
-    const today = new Date();
-    const appointments = [
-      { startsAt: today.setHours(12, 0) },
-      { startsAt: today.setHours(13, 0) }
-    ];
     render(<AppointmentsDayView appointments={appointments} />);
     expect(container.querySelectorAll("li")).toHaveLength(2);
     expect(container.querySelectorAll("li")[0].textContent).toEqual("12:00");
