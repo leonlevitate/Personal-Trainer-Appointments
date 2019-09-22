82697d19483a50ae6ddad87b8e716972404f7595
Leon Linton <leon.linton@live.co.uk>
2019-09-22 15:25:42 +0100

    Renders another appointment when selected

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const appointmentTimeofDay = startsAt => {
   const [h, m] = new Date(startsAt).toTimeString().split(":");
@@ -9,22 +9,25 @@ export const Appointment = ({ client: { firstName } }) => (
   <div>{firstName}</div>
 );
 
-export const AppointmentsDayView = ({ appointments }) => (
-  <div id="appointmentsDayView">
-    <ol>
-      {appointments.map(appointment => (
-        <li key={appointment.startsAt}>
-          <button type="button">
-            {appointmentTimeofDay(appointment.startsAt)}
-          </button>
-        </li>
-      ))}
-    </ol>
-    ;
-    {appointments.length === 0 ? (
-      <p>There are no Personal Training sessions scheduled for today</p>
-    ) : (
-      <Appointment {...appointments[0]} />
-    )}
-  </div>
-);
+export const AppointmentsDayView = ({ appointments }) => {
+  const [selectedAppointment, setSelectedAppointment] = useState(0);
+  return (
+    <div id="appointmentsDayView">
+      <ol>
+        {appointments.map((appointment, i) => (
+          <li key={appointment.startsAt}>
+            <button type="button" onClick={() => setSelectedAppointment(i)}>
+              {appointmentTimeofDay(appointment.startsAt)}
+            </button>
+          </li>
+        ))}
+      </ol>
+      ;
+      {appointments.length === 0 ? (
+        <p>There are no Personal Training sessions scheduled for today</p>
+      ) : (
+        <Appointment {...appointments[selectedAppointment]} />
+      )}
+    </div>
+  );
+};
