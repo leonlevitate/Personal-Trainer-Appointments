1e18c5ed158000094aede584c7f32cf0c580aece
leon.linton@live.co.uk <leon.linton@live.co.uk>
2020-02-01 13:10:49 +0000

    Adds service dropdowns

--- src/AppointmentForm.js
+++ src/AppointmentForm.js
@@ -1,35 +1,26 @@
 import React from 'react';
 
-export const AppointmentForm = () => (
+export const AppointmentForm = ({ selectableServices }) => (
   <form id="appointment">
-    <select name="service" />
+    <select name="service">
+      <option />
+      {selectableServices.map(s => (
+        <option key={s}>{s}</option>
+      ))}
+    </select>
   </form>
 );
 
-import React from 'react';
-import { createContainer } from './domManipulators';
-import { AppointmentForm } from '../src/AppointmentForm';
+AppointmentForm.defaultProps = {
+  selectableServices: [
+    'Cardiovascular',
+    'HIIT',
+    'Strength Training',
+    'Abs',
+    'Yoga',
+    'Lifts'
+  ]
+};
+  </form>
+);
 
-describe('AppointmentForm', () => {
-  let render, container;
-
-  beforeEach(() => {
-    ({ render, container } = createContainer());
-  });
-
-  const form = id => container.querySelector(`form[id="${id}"]`);
-  const field = name => form('appointment').elements[name];
-
-  it('renders a form', () => {
-    render(<AppointmentForm />);
-    expect(form('appointment')).not.toBeNull();
-  });
-
-  describe('service field', () => {
-    it('renders as a select box', () => {
-      render(<AppointmentForm />);
-      expect(field('service')).not.toBeNull();
-      expect(field('service').tagName).toEqual('SELECT');
-    });
-  });
-});
